76fbc35590c84a271d58391668f9665ecf3c42a6
Alex Milroy <amilroy@gmail.com>
2020-09-22 21:15:06 -0700

    finished alignment, working next on styling

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 //========================================================================
 // Global Variables
 //========================================================================
-var city = "seattle";
-var cities = ["Philadelphia"];
+var city = "";
+var cities = [];
 var lat = ""
 var long = ""
+var keyCounter = 0
 var currentDate = moment().format('MMMM Do YYYY, h:mm a');
 var apiKey = "907c23e4987ec932c6e00aa83a52deef"
 
@@ -17,23 +18,25 @@ var apiKey = "907c23e4987ec932c6e00aa83a52deef"
 
 
 function getCurrentWeather() {
+
     var currentQueryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
     $.ajax({
         url: currentQueryURL,
         method: "GET"
     }).then(function (coordResponse) {
         console.log(coordResponse)
+
         //get lat and long for oneCall
         var latCall = coordResponse.coord.lat;
         var longCall = coordResponse.coord.lon;
-        console.log(latCall);
-        console.log(longCall);
         lat = latCall;
         long = longCall;
-        var cityName = coordResponse.name;
-        var cityDiv = $("<div>");
-        cityDiv.text(cityName);
-        $("#city-name").append(cityDiv);
+
+        //Get the city name from response
+        var cityResponse = coordResponse.name;
+        $("#city-name").text(cityResponse);
+
+        
 
 
         var oneCallURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&exclude=hourly&appid=" + apiKey;
@@ -42,8 +45,6 @@ function getCurrentWeather() {
         method: "GET"
     }).then(function (response) {
         console.log(response)
-        //TODO: Get the city name from response
-        
 
         //TODO: Get the current date from the response
         var currentDate = response.timezone;
@@ -62,26 +63,13 @@ function getCurrentWeather() {
         $("#humidity").text("Humidity: " + currentHumid + "%");
 
         //Show the wind speed
-        var currentWind = response.current.wind_speed
+        var currentWind = response.current.wind_speed;
         $("#windspeed").text("Windspeed: " + currentWind + " mph");
+
+        //Show the UV Index
+        var uvIdx = response.current.uvi;
+        $("#uv").text("UV Index: " + uvIdx);
     });
-
-    
-
-        //========================================================================
-        // UV Index API Call
-        //========================================================================
-
-        //TODO: show the UV index
-        // var currentUvURL = "http://api.openweathermap.org/data/2.5/uvi?&lat=" + lat + "lon=" + long + "&appid=907c23e4987ec932c6e00aa83a52deef";
-        // $.ajax({
-        //     url: currentUvURL,
-        //     method: "GET"
-        // }).then(function (responseUV) {
-        //     console.log(responseUV);
-        //     var uvIdx = responseUV.value
-        //     $("#uv").text("UV Index: " + uvIdx); //reuse above
-        // });
 
         //========================================================================
         // 5 day forecast
@@ -94,6 +82,7 @@ function getCurrentWeather() {
             url: fiveDayURL,
             method: "GET"
         }).then(function (response) {
+            $("#five-day").empty();
             console.log(response)
             var fiveDayArray = ["0", "7", "14", "21", "28", "35"]
             var fiveIcon
@@ -107,23 +96,24 @@ function getCurrentWeather() {
 
 
                 //TODO: build the card
-                let cardBuilder = $("<div class=\"card\" style=\"width: 8rem\">");
+                let cardBuilder = $("<div class=\"card\" style=\"width: 4rem\">");
                 let divMaker = $("<div class=\"card-body\">");
+                
                 //TODO: Assign a date to this day
                 let dateMaker = $("<h3>")
                 dateMaker.text(futureDate);
 
 
-                //    //TODO: Assign an icon to this day
+                //TODO: Assign an icon to this day
 
-                //    let iconMaker = $("<div>")
 
-                //TODO: Assign the temperature to this day
+                // Assign the temperature to this day
                 var fiveDayTemp = response.list[fiveDayArray[days]].main.temp;
                 fiveDayTemp = Math.floor((fiveDayTemp - 273.15) * 1.8) + 32;
                 let tempMaker = $("<p>");
                 tempMaker.text("Temperature: " + fiveDayTemp + "°F");
-                //TODO: Assign the humidity to this day
+
+                // Assign the humidity to this day
                 var fiveDayHumid = response.list[fiveDayArray[days]].main.humidity;
                 let humidMaker = $("<P>")
                 humidMaker.text("Humidity: " + fiveDayHumid + "%")
@@ -151,17 +141,21 @@ function renderCities() {
     $("#city-list").empty();
     for (let i = 0; i < cities.length; i++) {
         var cityButton = $("<a>")
-        cityButton.addClass("list-group-item list-group-item-action")
-        cityButton.attr("data-name", cities[i]);
-        cityButton.text(cities[i]);
-        $("city-list").append(cityButton);
+        cityButton.addClass("w-75 p-3 list-group-item list-group-item-action")
+        cityButton.attr("id", cities[i]);
+        cityButton.text(localStorage.getItem(i))
+        $("#city-list").append(cityButton);
     }
 }
 
-$("#add-city").on("click", function (event) {
-    event.preventDefault();
-    var city = $("#cities-input").val().trim();
-    cities.push(city);
+$("#search-button").on("click", function(event) {
+    event.preventDefault()
+    var cityString = $("#searchfield").val().trim();
+    city = cityString
+    localStorage.setItem(keyCounter, cityString)
+    keyCounter++;
+    cities.push(cityString)
+    console.log(cities);
     renderCities();
     getCurrentWeather();
 })
